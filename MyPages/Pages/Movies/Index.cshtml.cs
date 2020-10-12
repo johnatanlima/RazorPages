7205f1910cfe1a4c3eee6a0c7cbd89dfdcd793c6
Johnatan Lima <johw.23@outlook.com>
2020-10-12 12:44:08 -0300

    Adicionados filtro e pesquisa

--- MyPages/Pages/Movies/Index.cshtml.cs
+++ MyPages/Pages/Movies/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyPages.Data;
 using MyPages.Models;
@@ -17,10 +20,40 @@ namespace MyPages.Pages.Movies
         }
 
         public IList<MoviesModel> Movies { get;set; }
+       
+        [BindProperty(SupportsGet = true)]
+        public string StrPesquisa { get; set; }
+     
+        public SelectList ListaGeneros { get; set; }
+       
+        [BindProperty(SupportsGet = true)]
+        public string Genero { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movies = await _context.Movies.ToListAsync();
+            IQueryable<string> listaGeneros = from m in _context.Movies
+                                            orderby m.Genre
+                                            select m.Genre;
+
+
+            var movies = from m in _context.Movies
+                                    select m;
+
+            if (!string.IsNullOrEmpty(StrPesquisa))
+            {
+                movies = movies.Where(s => s.Title.Contains(StrPesquisa));
+            }
+
+            if (!string.IsNullOrEmpty(Genero))
+            {
+                movies = movies.Where(x => x.Genre == Genero);
+            }
+
+            ListaGeneros = new SelectList(
+                await listaGeneros.Distinct().ToListAsync()
+            );
+
+            Movies = await movies.ToListAsync();
 
             ViewData["Detalhe"] = "Detalhe da mensagem indo para view!";
         }
